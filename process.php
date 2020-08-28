75acbf8acfb60ef6892ae0ffffb1c831b7892a65
0-fps <64110582+0-fps@users.noreply.github.com>
2020-08-27 22:39:32 -0300

    Update process.php

--- process.php
+++ process.php
@@ -1,13 +1,13 @@
 <?php
 
 
-  $connection = mysqli_connect("localhost", "root", "", "base");
+  $connection = mysqli_connect("localhost", "root", "", "base"); // credentials of our database
 
   $username = $_POST['username'];
   $password = $_POST['password'];
 
 
-  $sql = "SELECT usuario, pass FROM usuarios WHERE usuario = '$username' and pass = '$password'";
+  $sql = "SELECT usuario, pass FROM usuarios WHERE usuario = '$username' and pass = '$password'"; // syntax SQL for our database
   $result = mysqli_query($connection, $sql);
   $array = mysqli_fetch_array($result);
 
