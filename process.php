482439b26e692e1b8814175c43044913f0ea9b81
0-fps <64110582+0-fps@users.noreply.github.com>
2020-08-27 22:41:34 -0300

    Update process.php

--- process.php
+++ process.php
@@ -8,13 +8,13 @@
 
 
   $sql = "SELECT usuario, pass FROM usuarios WHERE usuario = '$username' and pass = '$password'"; // syntax SQL for our database
-  $result = mysqli_query($connection, $sql);
-  $array = mysqli_fetch_array($result);
+  $result = mysqli_query($connection, $sql); // Request the data from our database 
+  $array = mysqli_fetch_array($result); // Save the data in $array
 
-  if($array['usuario'] == $username && $array['pass'] == $password){
-    header("location: correcto.png");
+  if($array['usuario'] == $username && $array['pass'] == $password){ // if login is right or not
+    header("location: correcto.png"); // Right Login
   }else{
-    header("location: incorrecto.png");
+    header("location: incorrecto.png"); // Wrong Login
   }
 
  ?>
